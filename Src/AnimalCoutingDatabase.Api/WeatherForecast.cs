ac304c4b6e58af3891a9128a55c666f5409b3220
Giampaolo TUCCI <g.tucci@trilogik.it>
2022-07-23 17:49:37 +0200

    wip

--- Src/AnimalCoutingDatabase.Api/WeatherForecast.cs
+++ Src/AnimalCoutingDatabase.Api/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace TestGithubAction
+using System;
+
+namespace AnimalCoutingDatabase.Api
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace TestGithubAction
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
